802bc9c9743a3fee8de338735318a31cf75f2db8
Ron <ron_rose@me.com>
2023-06-12 11:37:07 -0400

    added body parser

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const routes = require('./routes/AppRouter')
 const db = require('./db')
+const bodyParser = require('body-parser')
 const logger = require('morgan')
 
 const PORT = process.env.port || 3001
 const app = express()
 
 app.use(express.json())
+app.use(bodyParser.json())
 app.use(logger('dev'))
 
 app.use(express.static(`public`))
